636451ee3f54bc25cb6a766ee5b5f66a10cc461a
katatsumuri1 <styahuoku@gmail.com>
2021-02-03 13:12:10 +0900

    finish chap4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,12 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 7;
+		String num1;
+		num1 = "Saiki";
+		System.out.println(num1);
+		num1 = "失敗は誰にでもあるが、大切なことは次同じ失敗を繰り返さないこと";
+		System.out.println(num1);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
