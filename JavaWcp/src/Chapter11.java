c1d732cebbc5dd19061caee30099fdf965b4c6c9
katatsumuri1 <styahuoku@gmail.com>
2021-02-13 16:57:19 +0900

    finish java_chap11

---
+++ JavaWcp/src/Chapter11.java
@@ -0,0 +1,29 @@
+
+public class Chapter11 {
+	public static void main (String[] args) {
+		//クラスのメンバーは、何と何ですか？
+		System.out.println("フィールド、メソッド");
+;		//アクセス修飾子は、何種類ありますか？
+		System.out.println("4種類");
+		//アクセス修飾子で、クラスに定義できるのは何と何ですか？
+		System.out.println("public、指定なし");
+		//インスタンス生成時に呼び出されるメソッドを何と呼びますか？
+		System.out.println("コンストラクタ");
+		//ディレクトリ階層でクラスをグループ化するしくみを何と呼びますか？
+		System.out.println("パッケージ");
+		//クラスで定義された変数を何変数と呼びますか？
+		System.out.println("メンバー変数");
+		//メソッド内で定義された変数を何変数と呼びますか？
+		System.out.println("ローカル変数");
+		//クラスから直接呼び出しができるメソッドを何と呼びますか？
+		System.out.println("staticメソッド");
+		//チェックする必要がない例外を何と呼びますか？
+		System.out.println("ランタイム例外");
+		//オブジェクト指向は、プログラムを何に見立ててプログラミングしますか？
+		System.out.println("もの");
+		//Javaで「型」というと、何を指しますか？
+		System.out.println("クラス");
+		//クラスを実体化したものを何と呼びますか？
+		System.out.println("インスタンス");
+	}
+}
